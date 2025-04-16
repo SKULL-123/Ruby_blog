c15ba52ebd304c4c960ec420850b3f2d4da5f11b
SKULL-123 <91775607+SKULL-123@users.noreply.github.com>
2025-04-16 10:58:44 +0300

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, :dependent => :delete_all 
   validates :title, :content, presence: true
   validates :slug, presence: true, uniqueness:true
 
